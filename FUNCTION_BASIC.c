9ae6936fb428b1c493d166e7cab895a646119c6e
angad <angadreact01@gmail.com>
2021-12-30 09:35:35 -0700

    first commit

---
+++ FUNCTION_BASIC.c
@@ -0,0 +1,16 @@
+// FUNCTION BASIC C PROGRAME
+#include <stdio.h>
+void display();//function prototype
+int main(){
+    int a;
+    printf("Virat kohli -->how are you\n");
+    display();//function call
+    printf("Angad -->how are you\n");
+
+
+    return 0;
+}
+//function defination
+void display(){
+    printf("Angad -->I am fine\n");
+}
